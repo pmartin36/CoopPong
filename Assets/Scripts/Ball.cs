609cc3b349db2a2b69fca807e8e2fa3dab672ecd
Paul Martin <pmartin@osisoft.com>
2018-10-29 22:44:01 -0400

    curving working well

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
 	Vector3 movementDirection;
-	private float BaseSpeed = 20;
+	private float BaseSpeed = 10;
 	public float MoveSpeed { get; set; }
 	public float Rotation { get; set; }
 
@@ -16,6 +16,9 @@ public class Ball : MonoBehaviour
 	private LayerMask collidableLayermask;
 	private CircleCollider2D ccollider;
 	private float castRadius;
+
+	private Vector3 lastPosition;
+	private RaycastHit2D hitBetweenLastMove;
 
 	// Start is called before the first frame update
 	void Start()
@@ -30,105 +33,102 @@ public class Ball : MonoBehaviour
     void FixedUpdate()
     {
 		Rotation -= (Rotation * 0.4f * Time.fixedDeltaTime);
-		curve += Rotation / 2000f * MoveSpeed * Time.deltaTime;
+		curve += Rotation / 200f * Time.deltaTime;
 		Vector3 curveToAdd =  isCurving ? curve * curveDirection : Vector3.zero;
 		transform.Rotate(0, 0, Rotation * Time.fixedDeltaTime);
 
-		var idealMove = (movementDirection * MoveSpeed + curveToAdd) * Time.fixedDeltaTime;
-		transform.position += idealMove;
+		transform.position += (movementDirection * MoveSpeed + curveToAdd) * Time.fixedDeltaTime; ;
 
-		//Vector3 actualDir = idealMove.normalized;
-		//float actualMS = idealMove.magnitude;
-		//RaycastHit2D hit = Physics2D.CircleCast(transform.position, castRadius, actualDir, actualMS, collidableLayermask);
-		//if (hit.collider == null) {
-		//	transform.position += idealMove;
-		//}
-		//else {
-		//	Debug.Log($"About to collide @{Time.time}");
-		//	//transform.position += actualDir * hit.distance;
-		//	HandleCollision(hit, actualDir, actualMS);
-		//}
+		hitBetweenLastMove = Physics2D.Linecast(lastPosition, transform.position, collidableLayermask);
+		lastPosition = transform.position;
 	}
 
-	public void HandleCollision(RaycastHit2D hit, Vector3 actualMoveDirection, float actualMS) {
-		Vector3 normal = hit.normal;
-		float dot = Vector2.Dot(actualMoveDirection, normal);
-		var inc = actualMoveDirection; // For debug
-		Debug.Log($"Collided with {hit.collider.name}, normal: { normal}, incoming: {inc}, dot: { dot}, new: {movementDirection}");
+	//public void HandleCollision(RaycastHit2D hit, Vector3 actualMoveDirection, float actualMS) {
+	//	Vector3 normal = hit.normal;
+	//	float dot = Vector2.Dot(actualMoveDirection, normal);
+	//	var inc = actualMoveDirection; // For debug
+	//	Debug.Log($"Collided with {hit.collider.name}, normal: { normal}, incoming: {inc}, dot: { dot}, new: {movementDirection}");
 
-		if (dot < 0) {	
-			if (Mathf.Min(normal.x, normal.y) > 0.01f && dot < -0.90f) {
-				actualMoveDirection *= -1f;
-			}
-			else {
-				movementDirection = actualMoveDirection - 2 * dot * normal;
-			}
+	//	if (dot < 0) {	
+	//		if (Mathf.Min(normal.x, normal.y) > 0.01f && dot < -0.90f) {
+	//			actualMoveDirection *= -1f;
+	//		}
+	//		else {
+	//			movementDirection = actualMoveDirection - 2 * dot * normal;
+	//		}
 
-			if (Mathf.Abs(movementDirection.x) < 0.25f) {
-				Debug.Log("Artifically adding x direction");
-				movementDirection = (movementDirection + Mathf.Sign(transform.position.x) * Vector3.left).normalized;
-			}
+	//		if (Mathf.Abs(movementDirection.x) < 0.25f) {
+	//			Debug.Log("Artifically adding x direction");
+	//			movementDirection = (movementDirection + Mathf.Sign(transform.position.x) * Vector3.left).normalized;
+	//		}
 
-			if (hit.collider.tag == "Player") {
-				var player = hit.collider.GetComponent<Player>();
-				Rotation += player.YMove * 60 * -Mathf.Sign(movementDirection.x);
+	//		if (hit.collider.tag == "Player") {
+	//			var player = hit.collider.GetComponent<Player>();
+	//			Rotation += player.YMove * 60 * -Mathf.Sign(movementDirection.x);
 
-				// curveDirection = movementDirection.Rotate(-90 * Mathf.Sign(movementDirection.x));
-				curveDirection = new Vector3(0, Mathf.Sign(movementDirection.x));
-			}
+	//			// curveDirection = movementDirection.Rotate(-90 * Mathf.Sign(movementDirection.x));
+	//			curveDirection = new Vector3(0, Mathf.Sign(movementDirection.x));
+	//		}
 
-			curve = 0;
-			float absDot = Mathf.Abs(Vector2.Dot(curveDirection, movementDirection));
-			isCurving = absDot < 0.7f;
-			Debug.Log("Rotation: {Rotation}, AbsDot: {absDot}, Rotation: {isCurving}");
+	//		curve = 0;
+	//		float absDot = Mathf.Abs(Vector2.Dot(curveDirection, movementDirection));
+	//		isCurving = absDot < 0.7f;
+	//		Debug.Log("Rotation: {Rotation}, AbsDot: {absDot}, Rotation: {isCurving}");
 
-			transform.position += actualMoveDirection * (hit.distance);
-			MoveSpeed += 0.25f;
-		}
-	}
+	//		transform.position += actualMoveDirection * (hit.distance);
+	//		MoveSpeed += 0.25f;
+	//	}
+	//}
 
 	private void GenerateRandomPositionAndDirection() {
 		MoveSpeed = BaseSpeed;
 		transform.position = new Vector2(0, Random.Range(-8f, 8f));
 		movementDirection = new Vector2(Random.Range(0.5f, 1f), Random.Range(0f, 0.4f)).normalized;
 		Rotation = Random.Range(-90f, 90f);
+		curve = 0;
 	}
 
 	public void OnCollisionEnter2D(Collision2D collision) {
-		Vector3 normal = collision.GetContact(0).normal;
+		Vector3 normal = hitBetweenLastMove.collider == collision.collider 
+			? hitBetweenLastMove.normal 
+			: collision.GetContact(0).normal;
 
 		Vector3 curveToAdd = isCurving ? curve * curveDirection : Vector3.zero;
 		Vector3 actualMoveDirection = (movementDirection * MoveSpeed + curveToAdd).normalized;
 
 		float dot = Vector2.Dot(actualMoveDirection, normal);
 
+		var inc = actualMoveDirection; // For debug	
+
 		if (dot < 0) {
-			var inc = actualMoveDirection; // For debug
 			if (Mathf.Min(normal.x, normal.y) > 0.01f && dot < -0.90f) {
-				actualMoveDirection *= -1f;
+				movementDirection = -actualMoveDirection;
 			}
 			else {
 				movementDirection = actualMoveDirection - 2 * dot * normal;
 			}
 
 			if (Mathf.Abs(movementDirection.x) < 0.25f) {
-				Debug.Log("Artifically adding x direction");
+				Debug.Log("Override");
 				movementDirection = (movementDirection + Mathf.Sign(transform.position.x) * Vector3.left).normalized;
 			}
-			Debug.Log($"normal: {normal}, incoming: {inc}, dot: {dot}, new: {movementDirection}");
 
 			if (collision.collider.tag == "Player") {
 				var player = collision.collider.GetComponent<Player>();
-				Rotation += player.YMove * 60 * -Mathf.Sign(movementDirection.x);
+				Rotation += player.YMove * 6 * MoveSpeed * -Mathf.Sign(movementDirection.x);
 
-				curveDirection = new Vector3(0, Mathf.Sign(movementDirection.x));
-				Debug.Log($"Collided with {collision.collider.name} @{Time.time}, Rotation: {Rotation}, Rotation: {isCurving}");
+				curveDirection = movementDirection.Rotate(90);
+				if( Mathf.Sign(curveDirection.x * Rotation) * Mathf.Sign(movementDirection.x) < 0 ) {
+					curveDirection.x = 0;
+				}
+				// Debug.Log($"Collided with {collision.collider.name} @{Time.time}, Rotation: {Rotation}, Rotation: {isCurving}");
 			}
 
 			curve = 0;
 			isCurving = Mathf.Abs(Vector2.Dot(curveDirection, movementDirection)) < 0.5f;
 			MoveSpeed += 0.25f;
 		}
+		Debug.Log($"normal: {normal}, incoming: {inc}, dot: {dot}, new: {movementDirection}");
 	}
 
 	public void OnCollisionStay2D(Collision2D collision) {
